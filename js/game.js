8d47920d874353ba2967f1b9fed94066990f0866
rayGreycloud <rayGreycloud@gmail.com>
2017-07-04 09:08:58 -0700

    Add game skeleton

---
+++ js/game.js
@@ -0,0 +1,34 @@
+// game.js
+// Set game parameters
+let width = 800;
+let height = 500;
+let renderer = 'Phaser.AUTO';
+let parent = 'gameDiv';
+let state = {
+    preload,
+    create,
+    update
+  };
+
+// Create game
+let game = new Phaser.Game(width, height, renderer, parent, state);
+
+// Initialize app variables
+
+
+// Preload state
+function preload() {
+
+}
+
+// Create state
+function create() {
+
+}
+
+// Update state
+function update() {
+
+}
+
+// Helper functions
